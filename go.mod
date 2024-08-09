d6571cca0ca0ff3d7ac1b1be9223229673d56076
Fouche du Preez <fouche@limehouse.co.za>
2024-08-09 14:13:10 +0200

    added registration persistence

--- go.mod
+++ go.mod
@@ -6,6 +6,6 @@ require (
 	github.com/a-h/templ v0.2.747
 	github.com/go-chi/chi/v5 v5.1.0
 	github.com/mattn/go-sqlite3 v1.14.22
+	github.com/qustavo/dotsql v1.2.0
+	golang.org/x/crypto v0.26.0
 )
-
-require github.com/qustavo/dotsql v1.2.0 // indirect
--- repo/sqliteRepo.go
+++ repo/sqliteRepo.go
@@ -2,6 +2,7 @@ package repo
 
 import (
 	"github.com/fouched/go-flaskr/models"
+	"golang.org/x/crypto/bcrypt"
 )
 
 func InsertUser(u models.User) error {
@@ -9,10 +10,27 @@ func InsertUser(u models.User) error {
 	stmt, _ := db.Prepare("INSERT INTO user(username, password) VALUES(?, ?)")
 	defer stmt.Close()
 
-	_, err := stmt.Exec(u.Email, u.Password)
+	hashedPwd, err := HashPassword(u.Password)
+	if err != nil {
+		return err
+	}
+
+	_, err = stmt.Exec(u.Email, hashedPwd)
 	if err != nil {
 		return err
 	}
 
 	return nil
+}
+
+// HashPassword generates a bcrypt hash for the given password.
+func HashPassword(password string) (string, error) {
+	bytes, err := bcrypt.GenerateFromPassword([]byte(password), 14)
+	return string(bytes), err
+}
+
+// VerifyPassword verifies if the given password matches the stored hash.
+func VerifyPassword(password, hash string) bool {
+	err := bcrypt.CompareHashAndPassword([]byte(hash), []byte(password))
+	return err == nil
 }
